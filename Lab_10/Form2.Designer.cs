149d9277d10b7b5b08827622e9c72e79ab1fb6d2
ByakuByaku <Ildarus.007@mail.ru>
2025-05-19 18:44:50 +0300

    Add a couple of windows forms

---
+++ Lab_10/Form2.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Lab_10
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(22, 50);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(212, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(22, 126);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(38, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(22, 203);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(38, 27);
+            textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(22, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(199, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Введите название лотереи:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(22, 103);
+            label2.Name = "label2";
+            label2.Size = new Size(233, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Введите количество участников:\r\n";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(22, 180);
+            label3.Name = "label3";
+            label3.Size = new Size(212, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Введите количество билетов:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(112, 270);
+            button1.Name = "button1";
+            button1.Size = new Size(195, 29);
+            button1.TabIndex = 6;
+            button1.Text = "Запустить лотерею";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Info;
+            ClientSize = new Size(407, 311);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form2";
+            Text = "Новая лотерея";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+    }
+}
---
+++ Lab_10/lotteryArchive.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Lab_10
+{
+    partial class lotteryArchive
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Honeydew;
+            button1.Location = new Point(175, 258);
+            button1.Name = "button1";
+            button1.Size = new Size(251, 81);
+            button1.TabIndex = 0;
+            button1.Text = "Новая лотерея";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.GradientInactiveCaption;
+            button2.Location = new Point(626, 258);
+            button2.Name = "button2";
+            button2.Size = new Size(251, 81);
+            button2.TabIndex = 1;
+            button2.Text = "Посмотреть статистику";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.MistyRose;
+            button3.ForeColor = SystemColors.ControlText;
+            button3.Location = new Point(396, 383);
+            button3.Name = "button3";
+            button3.Size = new Size(251, 81);
+            button3.TabIndex = 2;
+            button3.Text = "Таблица участников";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // lotteryArchive
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Info;
+            ClientSize = new Size(1062, 626);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "lotteryArchive";
+            Text = "lotteryArchive";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
